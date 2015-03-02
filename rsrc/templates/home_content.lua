2a3defa7ac252a6d12c61cdf23fd2209b672bebe
David Dalrymple (on zayin) <davidad@alum.mit.edu>
2015-03-02 05:43:22 +0000

    various unsynced improvements

--- rsrc/templates/home_content.lua
+++ rsrc/templates/home_content.lua
@@ -18,7 +18,7 @@ local social_links = {"davidad","eliana",
 
 local social_icons = {}
 for i,person in ipairs(social_links) do
-    table.insert(social_icons,[[<td>]])
+    table.insert(social_icons,[[<td><div>]])
     for j,link in ipairs(social_order) do
         table.insert(social_icons, {
             [[<a href="]],social_links[person][link],[[">]],
@@ -26,33 +26,29 @@ for i,person in ipairs(social_links) do
             [[</a>]]
         })
     end
-    table.insert(social_icons,[[</td>]])
+    table.insert(social_icons,[[</div></td>]])
 end
 
 local bio = [[
-  <p>eliana and davidad are traveling scientists/<wbr>programmers.</p>
+  <p>davidad and eliana are traveling scientists/<wbr>programmers.</p>
   <p>eliana is a <a href="http://en.wikipedia.org/wiki/Thiel_Fellowship">thiel fellow</a> and davidad is a research affiliate of the <a href="http://syntheticneurobiology.org">synthetic neurobiology</a> group at the <a href="http://web.mit.edu">mit</a> <a href="http://www.media.mit.edu">media lab</a>.</p>
   <p>while we mostly care about the timescale of decades, we like to play around with deep learning, write high-performance code, and explore interesting ideas. we don't like bad software and human aging and maybe we'll try to get rid of them.</p>
 ]]
 
 function M.render()
     return {
-        [[<main>]],
+        [[<main><div class="wrapper">]],
         [[<table>
-           <tr>
-              <td> davidad photo </td>
-              <td> eliana photo </td>
+           <tr class="headshots">
+              <td><img src="headshots/davidad-1024.jpg"></td>
+              <td><img src="headshots/eliana-1024.jpg"></td>
            </tr>
-           <tr>
-             <th> davidad </th>
-             <th> eliana  </th>
-           </tr>
-           <tr>]],
+           <tr class="social_icons">]],
               social_icons,
            [[</tr>
         </table>]],
         bio,
-        [[</main>]]
+        [[</div></main>]]
     }
 end
 
